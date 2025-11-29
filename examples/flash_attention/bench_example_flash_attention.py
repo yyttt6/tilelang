49ed74d200cb6807e23d407cc48105fdd6a9d741
yyttt6 <1652272478@qq.com>
2025-11-29 14:22:27 +0000

    format

--- examples/flash_attention/bench_example_flash_attention.py
+++ examples/flash_attention/bench_example_flash_attention.py
@@ -13,12 +13,15 @@ import example_mha_bwd_bshd
 import example_mha_bwd_bhsd
 import example_mha_bwd_bshd_wgmma_pipelined
 
+
 def bench_example_gqa_bwd_tma_reduce_varlen():
     tilelang.tools.bench.process_func(
         example_gqa_bwd_tma_reduce_varlen.benchmark, name="example_gqa_bwd_tma_reduce_varlen")
 
+
 def bench_example_gqa_bwd():
     tilelang.tools.bench.process_func(example_gqa_bwd.benchmark, name="example_gqa_bwd")
+
 
 def bench_example_gqa_bwd_wgmma_pipelined():
     tilelang.tools.bench.process_func(
@@ -49,6 +52,7 @@ def bench_example_gqa_fwd_bshd_wgmma_pipelined():
         groups=16,
         tune=False)
 
+
 def bench_example_gqa_fwd_bshd():
     tilelang.tools.bench.process_func(
         example_gqa_fwd_bshd.benchmark,
@@ -60,22 +64,28 @@ def bench_example_gqa_fwd_bshd():
         groups=16,
         tune=False)
 
+
 def bench_example_mha_fwd_bhsd_wgmma_pipelined():
     tilelang.tools.bench.process_func(example_mha_fwd_bhsd_wgmma_pipelined.benchmark)
 
+
 def bench_example_mha_fwd_bhsd():
     tilelang.tools.bench.process_func(example_mha_fwd_bhsd.benchmark)
+
 
 def bench_example_mha_fwd_bshd_wgmma_pipelined():
     tilelang.tools.bench.process_func(
         example_mha_fwd_bshd_wgmma_pipelined.benchmark, batch=1, heads=32, seq_len=256)
 
+
 def bench_example_mha_fwd_bshd():
     tilelang.tools.bench.process_func(example_mha_fwd_bshd.benchmark, batch=1, seq_len=256)
+
 
 def bench_example_mha_fwd_varlen():
     tilelang.tools.bench.process_func(
         example_mha_fwd_varlen.benchmark, batch=4, heads=16, seq_len=512, dim=64)
+
 
 if globals().get("__name__") == "__main__":
     tilelang.tools.bench.main()
